7308198eb28aa0e0c76959c98bbc97440855ae12
dylan h <baolong281@gmail.com>
2025-01-05 18:52:40 -0600

    stuff

---
+++ Html.hs
@@ -0,0 +1,17 @@
+module Html (
+    Html,
+    Title,
+    Structure,
+    html_,
+    p_,
+    h1_,
+    render,
+    ol_,
+    ul_,
+    pre_,
+    h_,
+    empty_,
+)
+where
+
+import Html.Internal
